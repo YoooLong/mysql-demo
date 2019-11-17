e9b8f931467261a7be80d25155a3953d7356a621
zhangpanpan <zhangpan_work@163.com>
2019-11-17 14:35:31 +0800

    初始化

---
+++ springboot-mycat-demo/src/main/java/com/yooolong/demo/springbootmycat/SpringbootMycatApplication.java
@@ -0,0 +1,13 @@
+package com.yooolong.demo.springbootmycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMycatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMycatApplication.class, args);
+    }
+
+}
---
+++ springboot-mycat-demo/src/test/java/com/yooolong/demo/springbootmycat/SpringbootMycatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yooolong.demo.springbootmycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMycatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
